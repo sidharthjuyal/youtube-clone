ea2124d4a4671218e57bfe7c9eb1208846b9952d
sidharthjuyal <sid.juyal.34@gmail.com>
2025-06-29 00:08:07 +0530

    Project Setup
    Project Setup

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+    return (<h1>Hello!</h1>);
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);

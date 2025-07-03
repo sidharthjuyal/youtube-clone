5fe4d792ffaa57af23580098af84650c34d7c550
sidharthjuyal <sid.juyal.34@gmail.com>
2025-07-03 08:19:54 +0530

    Built Live Chat, Updated Watch Page
    Built Live Chat, Updated Watch Page

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,7 +2,9 @@ import { closeMenu } from "../utils/appSlice";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { useSearchParams } from "react-router";
-import Comments from "./CommentsContainer";
+import CommentsContainer from "./CommentsContainer";
+import LiveChat from "./LiveChat";
+import Scroll from "./Scroll";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
@@ -10,21 +12,36 @@ const WatchPage = () => {
   useEffect(() => {
     dispatch(closeMenu());
   });
+  const videoData = true;
   return (
-    <div className="flex flex-col p-5">
-      <div>
-        <iframe
-          width="1000"
-          height="550"
-          src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
+    <div className=" flex  flex-col   justify-start  w-full   p-4  gap-2">
+      <div className="flex flex-col md:flex-row w-full gap-1 border  ">
+        <div className="w-full">
+          <Scroll />
+          {!videoData ? (
+            <div className="w-full  h-full animate-ping bg-grap-400"></div>
+          ) : (
+            <div className="flex flex-col w-full">
+              <iframe
+                className="w-full  h-[200px]  md:h-screen"
+                src={`https://www.youtube.com/embed/${searchParams.get(
+                  "v"
+                )}?autoplay=1`}
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              ></iframe>
+              <div className="w-full py-2 text-xl border-t  text-gray-600 font-bold">
+                {videoData?.snippet?.title}
+              </div>
+            </div>
+          )}
+        </div>
+        <LiveChat />
       </div>
-      <div>
-        <Comments />
+
+      <div className=" w-full ">
+        <CommentsContainer />
       </div>
     </div>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,3 +10,5 @@ export const USER_ICON =
 export const YOUTUBE_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=";
 
 export const YOUTUBE_SEARCH_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const OFFSET_LIVE_CHAT = 25;
